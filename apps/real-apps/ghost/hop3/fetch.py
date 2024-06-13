90352841fa85f5043111a1bb0ab358706a0a95ec
Stefane Fermigier <sf@abilian.com>
2024-06-13 16:51:47 +0200

    apps: start work on new apps

---
+++ apps/real-apps/ghost/hop3/fetch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+from pathlib import Path
+from urllib.request import urlopen
+
+VERSION = "5.84.1"
+NAME = f"moin-{VERSION}"
+
+DOWNLOAD_URL = f"https://github.com/TryGhost/Ghost/archive/refs/tags/v{VERSION}.tar.gz"
+
+print("We are here:", Path.cwd())
+
+archive_path = Path('source.tar.gz')
+
+with urlopen(DOWNLOAD_URL) as response:
+    data = response.read()
+    archive_path.write_bytes(data)
+
+os.system(f"tar -xzf {archive_path}")
+
+os.system(f"cp -r {NAME}/* .")
+os.system(f"rm -rf {NAME}")
---
+++ apps/real-apps/ghost/readme.md
@@ -0,0 +1,9 @@
+# Ghost - a web CMS
+
+Ghost is a modern, open-source Content Management System (CMS) primarily designed for professional publishing. Founded by John O'Nolan and launched in 2013 after a successful Kickstarter campaign, Ghost has rapidly grown in popularity among content creators due to its minimalist design, focus on content, and robust features tailored for modern web publishing.
+
+
+## Reference
+
+- https://github.com/TryGhost/Ghost
+- https://ghost.org/
