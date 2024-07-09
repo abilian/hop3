e027b5b034300d21eaa7b6a9990f5efef3c870f5
Stefane Fermigier <sf@abilian.com>
2024-07-09 11:29:17 +0200

    chore: deps + check

---
+++ scripts/checkdb.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2023-2024, Abilian SAS
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+from hop3.model import metadata
+
+engine = create_engine("sqlite:///:memory:")
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+metadata.create_all(engine)
