b36e59d352f506de455081898a330be653c2545c
Stefane Fermigier <sf@abilian.com>
2024-07-24 17:48:24 +0200

    chore: remove code that has been move to a subproject

--- src/hop3_main/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Main server (aka manager / director / orchestrator) for the system.
-
-Also includes the Web front-end.
-
-This is a Flask app that serves as the main entry point for the system and will orchestrate the various components.
-"""
