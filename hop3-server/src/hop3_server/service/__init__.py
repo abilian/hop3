5801f0bcf31d17f801f9bb8d768cf77f1c41562b
Stefane Fermigier <sf@abilian.com>
2024-07-25 11:15:00 +0200

    doc: doctsring

--- hop3-server/src/hop3_server/service/__init__.py
+++ hop3-server/src/hop3_server/service/__init__.py
@@ -1,6 +1,13 @@
+"""
+This package contains the service layer of the hop3 server.
+
+Some of the API is currently duplicated from the `hop3_agent` package
+(this will be refactored in the future, as we move to a properly layered
+architecture).
+"""
 from __future__ import annotations
 
 from .api import get_app, list_apps
 from .model import App
 
-__all__ = ["list_apps", "get_app", "App"]
+__all__ = ["App", "get_app", "list_apps"]
