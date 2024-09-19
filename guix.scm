cbceba2d16a8c31557b02caf35be734a2db1fd8a
Stefane Fermigier <sf@abilian.com>
2024-09-19 08:54:19 +0200

    chore: basic Guix config.

--- guix.scm
+++ guix.scm
@@ -8,23 +8,24 @@
 ;; or:
 ;;    guix time-machine -C channels.scm -- build -f guix.scm
 
-(use-modules (guix build-system pyproject)
-             (guix git)
-             (guix gexp)
-             (guix packages)
-             (guix licenses)
-             (gnu packages))
 
-(include "manifest.scm")
+(use-modules (guix build-system pyproject)
+    (guix git)
+    (guix gexp)
+    (guix packages)
+    (guix licenses)
+    (gnu packages base))
+
+;; (include "manifest.scm")
 
 (package
-  (name "hop3")
-  (version "0.1.0")
-  (source (git-checkout (url (dirname (current-filename)))))
-  (build-system pyproject-build-system)
-  (propagated-inputs %packages)
-  (native-inputs %dev-packages)
-  (home-page "https://github.com/abilian/hop3")
-  (synopsis "xxx")
-  (description "xxx")
-  (license #f))
+    (name "hop3")
+    (version "0.1.0")
+    (source (git-checkout (url (dirname (current-filename)))))
+    (build-system pyproject-build-system)
+    ;; (propagated-inputs %packages)
+    ;; (native-inputs %dev-packages)
+    (home-page "https://github.com/abilian/hop3")
+    (synopsis "Simple PaaS - Deploy and manage web applications on a single server")
+    (description "Simple PaaS - Deploy and manage web applications on a single server")
+    (license agpl3))
