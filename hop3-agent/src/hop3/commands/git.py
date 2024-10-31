db7b7600be388fe5dddf0bc073f1fa665bcd19ca
Stefane Fermigier <sf@abilian.com>
2024-10-31 15:01:11 +0100

    refact: git commands

--- hop3-agent/src/hop3/commands/git.py
+++ hop3-agent/src/hop3/commands/git.py
@@ -9,18 +9,17 @@ from __future__ import annotations
 
 import subprocess
 import sys
-from textwrap import dedent
 
 from click import argument
 
 from hop3.core.app import App, get_app
+from hop3.core.git import GitManager
 from hop3.deploy import do_deploy
-from hop3.system.constants import APP_ROOT, GIT_ROOT, HOP3_ROOT, HOP3_SCRIPT
+from hop3.system.constants import APP_ROOT, GIT_ROOT
 from hop3.util import sanitize_app_name
 from hop3.util.console import echo
 
 from .cli import hop3
-from .util import make_executable
 
 
 @hop3.command("git-hook")
@@ -29,16 +28,16 @@ def cmd_git_hook(app_name: str) -> None:
     """INTERNAL: Post-receive git hook."""
     app = App(app_name)
 
+    # Handle pushes
+    if not app.app_path.exists():
+        echo(f"-----> Creating app '{app_name}'", fg="green")
+        app.create()
+
+        cmd = ["git", "clone", "--quiet", app.repo_path, app_name]
+        subprocess.run(cmd, cwd=APP_ROOT)
+
     for line in sys.stdin:
         _oldrev, newrev, _refname = line.strip().split(" ")
-
-        # Handle pushes
-        if not app.app_path.exists():
-            echo(f"-----> Creating app '{app_name}'", fg="green")
-            app.create()
-
-            cmd = ["git", "clone", "--quiet", app.repo_path, app_name]
-            subprocess.run(cmd, cwd=APP_ROOT)
 
         do_deploy(app_name, newrev=newrev)
 
@@ -49,25 +48,10 @@ def cmd_git_receive_pack(app_name: str) -> None:
     """INTERNAL: Handle git pushes for an app."""
     app_name = sanitize_app_name(app_name)
     app = get_app(app_name, check=False)
-    hook_path = app.repo_path / "hooks" / "post-receive"
 
-    if not hook_path.exists():
-        hook_path.parent.mkdir(parents=True)
-
-        # Initialize the repository with a hook to this script
-        cmd = ["git", "init", "--quiet", "--bare", app.name]
-        subprocess.run(cmd, cwd=GIT_ROOT)
-
-        hook_path.write_text(
-            dedent(
-                f"""\
-                #!/usr/bin/env bash
-                set -e; set -o pipefail;
-                cat | HOP3_ROOT="{HOP3_ROOT}" {HOP3_SCRIPT} git-hook {app.name}
-                """,
-            )
-        )
-        make_executable(hook_path)
+    # app.create()
+    git_manager = GitManager(app)
+    git_manager.setup_hook()
 
     # Handle the actual receive. We'll be called with 'git-hook' after it happens
     cmd = ["git-receive-pack", app.name]
---
+++ hop3-agent/src/hop3/core/git.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2023-2024, Abilian SAS
+
+"""
+Server-side git operations.
+"""
+
+import stat
+import subprocess
+from pathlib import Path
+from textwrap import dedent
+
+from attrs import frozen
+
+from hop3.system.constants import GIT_ROOT, HOP3_ROOT, HOP3_SCRIPT
+
+from .app import App
+
+
+@frozen
+class GitManager:
+    app: App
+
+    def setup_hook(self):
+        setup_hook(self.app)
+
+
+def setup_hook(app):
+    hook_path = app.repo_path / "hooks" / "post-receive"
+    if not hook_path.exists():
+        hook_path.parent.mkdir(parents=True)
+
+        # Initialize the repository with a hook to this script
+        cmd = ["git", "init", "--quiet", "--bare", app.name]
+        subprocess.run(cmd, cwd=GIT_ROOT)
+
+        hook_path.write_text(
+            dedent(
+                f"""\
+                #!/usr/bin/env bash
+                set -e; set -o pipefail;
+                cat | HOP3_ROOT="{HOP3_ROOT}" {HOP3_SCRIPT} git-hook {app.name}
+                """,
+            )
+        )
+        make_executable(hook_path)
+
+
+def make_executable(path: Path) -> None:
+    """Make file executable by the user."""
+    path.chmod(path.stat().st_mode | stat.S_IXUSR)
