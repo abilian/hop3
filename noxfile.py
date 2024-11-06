7af9cdf95b7496edc61cc45a695bd004f4fc82cf
Stefane Fermigier <sf@abilian.com>
2024-11-06 12:29:09 +0100

    chore: deps and CI scripts/config.

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 from __future__ import annotations
 
+from pathlib import Path
+
 import nox
 
 # Minimal version is 3.10
@@ -10,7 +12,6 @@ PYTHON_VERSIONS = ["3.10", "3.11", "3.12"]
 
 nox.options.reuse_existing_virtualenvs = True
 
-nox.options.reuse_existing_virtualenvs = True
 # nox.options.default_venv_backend = "venv"
 
 # Don't run 'update-deps' by default.
@@ -20,10 +21,19 @@ nox.options.sessions = [
     "doc",
 ]
 
+
+def list_subrepos() -> list[str]:
+    packages = Path("packages").iterdir()
+    return [str(d) for d in packages if d.is_dir()]
+
+
+# SUB_REPOS = list_subrepos()
+
 SUB_REPOS = [
     "packages/hop3-agent",
     "packages/hop3-server",
     "packages/hop3-cli",
+    "packages/hop3-testing",
 ]
 
 
@@ -58,13 +68,13 @@ def doc(session: nox.Session) -> None:
     print("TODO: do something with the docs")
 
 
-@nox.session(name="update-deps")
-def update_deps(session: nox.Session) -> None:
-    for sub_repo in SUB_REPOS:
-        with session.chdir(sub_repo):
-            session.run("poetry", "install", external=True)
-            session.run("poetry", "update", external=True)
-        print()
+# @nox.session(name="update-deps")
+# def update_deps(session: nox.Session) -> None:
+#     for sub_repo in SUB_REPOS:
+#         with session.chdir(sub_repo):
+#             session.run("poetry", "install", external=True)
+#             session.run("poetry", "update", external=True)
+#         print()
 
 
 def run_subsession(session, sub_repo) -> None:
