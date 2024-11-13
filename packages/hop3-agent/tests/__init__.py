a055c146f1874638af452001d469e3fe51e2e5b1
Stefane Fermigier <sf@abilian.com>
2024-11-13 18:24:39 +0100

    chore: update copyright

--- packages/hop3-agent/tests/__init__.py
+++ packages/hop3-agent/tests/__init__.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2024, Abilian SAS
 #
 # Don't remove, this file is needed to treat the directory as a package.
 #
--- scripts/update-copyright.py
+++ scripts/update-copyright.py
@@ -1,6 +1,8 @@
 import re
 from pathlib import Path
 
+from devtools import debug
+
 PAT_1Y = r"# Copyright \(c\) ([0-9]+), Abilian SAS"
 PAT_2Y = r"# Copyright \(c\) ([0-9]+)-([0-9]+), Abilian SAS"
 
@@ -15,8 +17,12 @@ def main():
 
 def update_copyright(python_file):
     content = python_file.read_text()
-    if not re.match(PAT_1Y, content) and not re.match(PAT_2Y, content):
-        content = f"# Copyright (c) {CURRENT_YEAR}, Abilian SAS\n\n" + content
+
+    m1 = re.search(PAT_1Y, content)
+    m2 = re.search(PAT_2Y, content)
+    if not m1 and not m2:
+        print(f"Adding copyright in {python_file}")
+        content = f"# Copyright (c) {CURRENT_YEAR}, Abilian SAS\n" + content
         python_file.write_text(content)
         return
 
@@ -28,21 +34,18 @@ def update_copyright(python_file):
 
         m = re.match(PAT_1Y, line)
         if m:
-            print(f"Updating copyright in {python_file}")
             year = int(m.group(1))
             if year == CURRENT_YEAR:
-                print(f"Skipping {python_file}")
                 return
 
+            print(f"Updating copyright in {python_file}")
             line = f"# Copyright (c) {year}-{CURRENT_YEAR}, Abilian SAS"
 
         m = re.match(PAT_2Y, line)
         if m:
-            print(f"Updating copyright in {python_file}")
             year_start = int(m.group(1))
             year_end = int(m.group(2))
             if year_end == CURRENT_YEAR:
-                print(f"Skipping {python_file}")
                 return
 
             line = f"# Copyright (c) {year_start}-{CURRENT_YEAR}, Abilian SAS"
