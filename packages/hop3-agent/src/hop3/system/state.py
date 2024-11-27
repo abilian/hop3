0f65f37d73b02a8c5ce8c4327ed991bed55c42a1
Stefane Fermigier <sf@abilian.com>
2024-11-27 17:54:26 +0100

    refact: remove dead code.

--- packages/hop3-agent/src/hop3/system/state.py
+++ packages/hop3-agent/src/hop3/system/state.py
@@ -9,7 +9,6 @@ Store the state of the system.
 
 from __future__ import annotations
 
-from hop3.config.constants import HOP3_ROOT
 from hop3.util.settings import parse_settings
 
 
@@ -31,29 +30,6 @@ class State:
         app = App(app_name)
         settings = app.virtualenv_path / "ENV"
         return parse_settings(settings)
-
-    def get_global(self, key: str):
-        """
-        Retrieve a global setting by key.
-
-        Input:
-        - key (str): The key for the setting to retrieve.
-
-        Returns:
-        - The value associated with the key in the global settings.
-
-        Raises:
-        - KeyError: If the key does not exist in the global settings.
-
-        The function accesses a global settings file and retrieves the value
-        associated with the provided key. It uses the `parse_settings` function
-        to read and parse the settings file located at 'GLOBAL' within the
-        HOP3_ROOT directory.
-
-        NB: Not used. Will be removed in a future commit.
-        """
-        settings = HOP3_ROOT / "GLOBAL"
-        return parse_settings(settings).get(key)
 
 
 state = State()
