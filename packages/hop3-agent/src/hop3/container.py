329e024e6755c56c6886dcb4a92da106bf4b9580
Stefane Fermigier <sf@abilian.com>
2025-03-19 17:53:16 +0100

    chore: cleanup commented out code.

--- packages/hop3-agent/src/hop3/container.py
+++ packages/hop3-agent/src/hop3/container.py
@@ -1,19 +1,11 @@
-# Copyright (c) 2024-2025, Abilian SAS
+# Copyright (c) 2025, Abilian SAS
 from __future__ import annotations
 
 from wireup import DependencyContainer, create_container
 
 from hop3 import services
-from hop3.config import get_parameters
-
-parameters = get_parameters()
 
 container: DependencyContainer = create_container(
-    # Parameters serve as application/service configuration.
-    # parameters={
-    #     "redis_url": os.environ["APP_REDIS_URL"],
-    #     "weather_api_key": os.environ["APP_WEATHER_API_KEY"],
-    # },
     # Let the container know where service registrations are located.
     service_modules=[services]
 )
