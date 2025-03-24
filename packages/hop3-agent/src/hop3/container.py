82b81273718fd138825e3f5576deb7ca697560e2
Stefane Fermigier <sf@abilian.com>
2025-03-24 11:46:25 +0100

    chore: deps + fix

--- packages/hop3-agent/src/hop3/container.py
+++ packages/hop3-agent/src/hop3/container.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2025, Abilian SAS
 from __future__ import annotations
 
-from wireup import DependencyContainer, create_container
+from wireup import create_sync_container
 
 from hop3 import services
 
-container: DependencyContainer = create_container(
+container = create_sync_container(
     # Let the container know where service registrations are located.
     service_modules=[services]
 )
