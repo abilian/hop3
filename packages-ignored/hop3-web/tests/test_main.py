b281cb33ada5f4334560a9e8202e70f6dade5099
Stefane Fermigier <sf@abilian.com>
2025-04-07 16:44:53 +0200

    refact: cleanup tests / fix warnings

--- packages-ignored/hop3-web/tests/test_main.py
+++ packages-ignored/hop3-web/tests/test_main.py
@@ -1,6 +1,12 @@
 # Copyright (c) 2023-2025, Abilian SAS
 from __future__ import annotations
 
+from hop3_web.web.main import create_app
 
-def test_dummy() -> None:
-    assert True
+from .conftest import TestConfig
+
+
+def test_create_app() -> None:
+    config = TestConfig()
+    app = create_app(config)
+    assert app
