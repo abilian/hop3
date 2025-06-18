1105a713c968d06ba2bb92c0f2fcbe7dc6a05629
Stefane Fermigier <sf@abilian.com>
2025-06-18 13:01:11 +0200

    feat: display fixed help message for now.

--- packages/hop3-server/src/hop3/commands/help.py
+++ packages/hop3-server/src/hop3/commands/help.py
@@ -9,6 +9,27 @@ from hop3.lib.registry import lookup, register
 
 from ._base import Command
 
+HELP = """
+COMMANDS
+  apps            List apps (running or stopped).
+  backup          Run a backup for an app.
+  config          Manage app config. Type 'hop config' for help.
+  deploy          Deploy app.
+  destroy         Destroy app, remove all files.
+  help            Display help information for the Hop3 CLI.
+  logs            Tail running logs, e.g: hop-agent logs <app> [<process>].
+  pg              Manage a PostgreSQL database.
+  plugins         List installed plugins.
+  ps              Show process count for app.
+  redis           Manage Redis commands.
+  restart         Restart an app.
+  run             Run command in the context of app, e.g.: hop run ls -- -al.
+  sbom            Generate a SBOM for an app.
+  setup           Initialize environment.
+  start           Stop an app.
+  stop            Stop an app.
+"""
+
 
 @register
 class HelpCmd(Command):
@@ -40,6 +61,10 @@ class HelpCmd(Command):
             help_text = cmd.__doc__ or ""
             output.append(f"  {name:<20} {help_text}")
 
+        # return [
+        #     {"t": "text", "text": "\n".join(output)},
+        # ]
+
         return [
-            {"t": "text", "text": "\n".join(output)},
+            {"t": "text", "text": HELP},
         ]
