3e249f0d360268b5dcb6c0739925f48dd482e3fb
Stefane Fermigier <sf@abilian.com>
2025-10-08 19:03:03 +0200

    feat: debug plugins

--- packages/hop3-server/src/hop3/plugins/services/redis/plugin.py
+++ packages/hop3-server/src/hop3/plugins/services/redis/plugin.py
@@ -1,15 +1,33 @@
 # Copyright (c) 2023-2025, Abilian SAS
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Redis plugin for Hop3.
+
+This plugin provides Redis service capabilities to Hop3 applications.
+"""
+
 from __future__ import annotations
 
+from hop3.core.hooks import hop3_hook_impl
+from hop3.core.protocols import ServiceStrategy
+
 from . import cli
+from .redis import RedisService
 
 assert cli
 
 
 class RedisPlugin:
-    """A placeholder class for a RedisPlugin.
+    """Redis service plugin for Hop3.
 
-    This currently does not have any implemented functionality or
-    attributes. It serves as a base or placeholder class and can be
-    extended in the future.
+    This plugin registers the Redis service strategy with the Hop3
+    plugin system, allowing applications to create and attach Redis instances.
     """
+
+    name = "redis"
+
+    @hop3_hook_impl
+    def get_service_strategies(self) -> list[type[ServiceStrategy]]:
+        """Return Redis service strategy."""
+        return [RedisService]
