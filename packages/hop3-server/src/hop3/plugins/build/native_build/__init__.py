de16135f6c6672e295f68908449222b0954da6fe
Stefane Fermigier <sf@abilian.com>
2025-11-25 13:51:59 +0100

    Fix test.

--- packages/hop3-server/src/hop3/plugins/build/native_build/__init__.py
+++ packages/hop3-server/src/hop3/plugins/build/native_build/__init__.py
@@ -1,1 +1,21 @@
 # Copyright (c) 2025, Abilian SAS
+#
+# SPDX-License-Identifier: Apache-2.0
+"""Native build plugin - registers all native language builders."""
+
+from __future__ import annotations
+
+from hop3.builders import BUILDER_CLASSES
+from hop3.core.hooks import hop3_hook_impl
+
+
+class NativeBuildPlugin:
+    """Plugin that provides native build strategies for various languages."""
+
+    @hop3_hook_impl
+    def get_build_strategies(self) -> list:
+        """Return all native builder classes."""
+        return BUILDER_CLASSES
+
+
+plugin = NativeBuildPlugin()
