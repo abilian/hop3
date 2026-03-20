11062ff784d6eb5717b4ac578b660e8c8bd86fe6
Stefane Fermigier <sf@abilian.com>
2026-03-20 16:54:40 +0100

    refact: cleanup

--- packages/hop3-installer/src/hop3_installer/bundler.py
+++ packages/hop3-installer/src/hop3_installer/bundler.py
@@ -49,6 +49,7 @@ SERVER_MODULES = [
     "server_installer/cli.py",
     "server_installer/user.py",
     "server_installer/deps_common.py",
+    "server_installer/docker_utils.py",
     "server_installer/redis.py",
     "server_installer/deps_debian.py",
     "server_installer/deps_fedora.py",
