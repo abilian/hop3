7b3c2c8760b3dc58aa434c8e9bf41573fcd1c51c
Stefane Fermigier <sf@abilian.com>
2026-03-24 18:31:21 +0100

    test: cleanup

--- packages/hop3-server/tests/d_e2e/test_simple_apps.py
+++ packages/hop3-server/tests/d_e2e/test_simple_apps.py
@@ -42,16 +42,6 @@ def test_app_deployment(app_dir: Path, deployment_target):
     """Test deployment of an application from apps/test-apps/."""
     app_name = app_dir.name
 
-    # FIXME later
-    if app_name == "030-golang-gin":
-        return
-
-    # FIXME: pyproject.toml apps fail with 502 when pip install . is used
-    # The pip install succeeds locally but gunicorn fails to start in container
-    # Needs investigation - might be related to working directory or PYTHONPATH
-    if app_name == "110-flask-gunicorn-poetry":
-        pytest.skip("pyproject.toml deployment needs investigation")
-
     app = AppSource(name=app_name, path=app_dir)
     with DeploymentSession(app, deployment_target) as session:
         try:
